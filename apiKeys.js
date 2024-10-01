85bc9f29a3a9bafa7761ee112e6669ffefba9c54
utkrshkr <151050784+utkrshkr@users.noreply.github.com>
2024-10-01 18:03:14 +0530

    Add files via upload

---
+++ apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "45e2dcbd72ce2fbb9f606f00db34e7db",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
